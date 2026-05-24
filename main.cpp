02410178e44d7db48fc9d3776f81089db3244470
PranauvCse2006 <pranauvsps@gmail.com>
2026-05-24 08:05:12 +0530

    Improved game board UI

--- main.cpp
+++ main.cpp
@@ -9,20 +9,22 @@ char board[3][3]=
 };
 void displayBoard(){
     cout<<endl;
+    cout << "     TIC TAC TOE "<<endl;
+    cout << "=======================\n\n";
     for(int i=0;i<3;i++){
-        cout<<" ";
+        cout<<"  ";
         for(int j=0;j<3;j++){
             cout<<board[i][j];
             if(j<2)
             {
-                cout<<" | ";
+                cout<<"  |  ";
             }
         }
         cout<<endl;
 
         if(i<2)
         {
-            cout<<"---|---|---"<<endl;
+            cout<<"-----|-----|----"<<endl;
         }
     }
     cout<<endl;
@@ -105,7 +107,7 @@ int main()
     {
         resetBoard();
 
-        move = 0;
+        moves = 0;
         player = 'X';
     
     displayBoard();
